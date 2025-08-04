cda9813abe10329223b97dcf70f362709089df6b
fpassanha <fpassanha@gmail.com>
2025-08-04 17:02:02 -0300

    update

---
+++ interpolacao_variaveis.py
@@ -0,0 +1,11 @@
+nome = "João"
+idade = 30
+print(f"Meu nome é {nome} e tenho {idade} anos.")
+
+nome = "Maria"
+idade = 25
+print("Meu nome é {0} e tenho {1} anos.".format(nome, idade))
+
+nome = "Carlos"
+idade = 40
+print("Meu nome é %s e tenho %d anos." % (nome, idade))
