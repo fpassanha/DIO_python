885fc2aa15ef1b75df34a77cc64215ec81619e7f
fpassanha <fpassanha@gmail.com>
2025-08-04 17:18:40 -0300

    updt

---
+++ fatiamento_string.py
@@ -0,0 +1,16 @@
+texto = "Python é divertido!"
+
+# Fatiando do início até o índice 6 (não inclui o 6)
+print(texto[:6])         # Python
+
+# Fatiando do índice 7 até o final
+print(texto[7:])         # é divertido!
+
+# Fatiando do índice 7 ao 9 (não inclui o 9)
+print(texto[7:9])        # é 
+
+# Fatiando com passo
+print(texto[::2])        # Pto édvri!
+
+# Fatiando os 7 últimos caracteres
+print(texto[-7:])
---
+++ string_triplas.py
@@ -0,0 +1,12 @@
+
+# Exemplo de menu com string tripla
+menu = """
+========= MENU =========
+  1 - Depositar
+  2 - Sacar
+  3 - Consultar saldo
+  0 - Sair
+=======================
+"""
+
+print(menu)
